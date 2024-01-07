0ee6a5b9625a392ec1a53cb95b0d4340d4bab20a
Matheus Estrela <mr.estrela@yahoo.com.br>
2024-01-07 10:43:31 -0300

    Mario64

---
+++ mario.py
@@ -0,0 +1,73 @@
+import collections, dataclasses, enum, io, itertools as it, pygame as pg, urllib.request
+from random import randint
+
+P = collections.namedtuple('P', 'x y')          # Position
+D = enum.Enum('D', 'n e s w')                   # Direction
+W, H, MAX_S = 50, 50, P(5, 10)                  # Width, Height, Max speed
+
+def main():
+    def get_screen():
+        pg.init()
+        return pg.display.set_mode((W*16, H*16))
+    def get_images():
+        url = 'https://gto76.github.io/python-cheatsheet/web/mario_bros.png'
+        img = pg.image.load(io.BytesIO(urllib.request.urlopen(url).read()))
+        return [img.subsurface(get_rect(x, 0)) for x in range(img.get_width() // 16)]
+    def get_mario():
+        Mario = dataclasses.make_dataclass('Mario', 'rect spd facing_left frame_cycle'.split())
+        return Mario(get_rect(1, 1), P(0, 0), False, it.cycle(range(3)))
+    def get_tiles():
+        border = [(x, y) for x in range(W) for y in range(H) if x in [0, W-1] or y in [0, H-1]]
+        platforms = [(randint(1, W-2), randint(2, H-2)) for _ in range(W*H // 10)]
+        return [get_rect(x, y) for x, y in border + platforms]
+    def get_rect(x, y):
+        return pg.Rect(x*16, y*16, 16, 16)
+    run(get_screen(), get_images(), get_mario(), get_tiles())
+
+def run(screen, images, mario, tiles):
+    clock = pg.time.Clock()
+    pressed = set()
+    while not pg.event.get(pg.QUIT) and clock.tick(28):
+        keys = {pg.K_UP: D.n, pg.K_RIGHT: D.e, pg.K_DOWN: D.s, pg.K_LEFT: D.w}
+        pressed |= {keys.get(e.key) for e in pg.event.get(pg.KEYDOWN)}
+        pressed -= {keys.get(e.key) for e in pg.event.get(pg.KEYUP)}
+        update_speed(mario, tiles, pressed)
+        update_position(mario, tiles)
+        draw(screen, images, mario, tiles, pressed)
+
+def update_speed(mario, tiles, pressed):
+    x, y = mario.spd
+    x += 2 * ((D.e in pressed) - (D.w in pressed))
+    x += (x < 0) - (x > 0)
+    y += 1 if D.s not in get_boundaries(mario.rect, tiles) else (D.n in pressed) * -10
+    mario.spd = P(x=max(-MAX_S.x, min(MAX_S.x, x)), y=max(-MAX_S.y, min(MAX_S.y, y)))
+
+def update_position(mario, tiles):
+    x, y = mario.rect.topleft
+    n_steps = max(abs(s) for s in mario.spd)
+    for _ in range(n_steps):
+        mario.spd = stop_on_collision(mario.spd, get_boundaries(mario.rect, tiles))
+        mario.rect.topleft = x, y = x + (mario.spd.x / n_steps), y + (mario.spd.y / n_steps)
+
+def get_boundaries(rect, tiles):
+    deltas = {D.n: P(0, -1), D.e: P(1, 0), D.s: P(0, 1), D.w: P(-1, 0)}
+    return {d for d, delta in deltas.items() if rect.move(delta).collidelist(tiles) != -1}
+
+def stop_on_collision(spd, bounds):
+    return P(x=0 if (D.w in bounds and spd.x < 0) or (D.e in bounds and spd.x > 0) else spd.x,
+             y=0 if (D.n in bounds and spd.y < 0) or (D.s in bounds and spd.y > 0) else spd.y)
+
+def draw(screen, images, mario, tiles, pressed):
+    def get_marios_image_index():
+        if D.s not in get_boundaries(mario.rect, tiles):
+            return 4
+        return next(mario.frame_cycle) if {D.w, D.e} & pressed else 6
+    screen.fill((85, 168, 255))
+    mario.facing_left = (D.w in pressed) if {D.w, D.e} & pressed else mario.facing_left
+    screen.blit(images[get_marios_image_index() + mario.facing_left * 9], mario.rect)
+    for t in tiles:
+        screen.blit(images[18 if t.x in [0, (W-1)*16] or t.y in [0, (H-1)*16] else 19], t)
+    pg.display.flip()
+
+if __name__ == '__main__':
+    main()
